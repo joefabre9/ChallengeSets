fc080b7d4e7537c5149ee088ae9a02e3e57c8b45
joe fabre <joefabre9@gmail.com>
2020-10-24 21:03:50 -0400

    update

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -5,7 +5,15 @@ namespace ChallengeSets
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            if (num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double Subtract(double minuend, double subtrahend)
