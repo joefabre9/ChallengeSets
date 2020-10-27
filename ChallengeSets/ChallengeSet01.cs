c3e3c44792e85e59f858b3d000442b851fafd49b
joe fabre <joefabre9@gmail.com>
2020-10-26 22:56:11 -0400

    Challenge Set 1 Completed

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -18,27 +18,55 @@ namespace ChallengeSets
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            var sub1 = minuend - subtrahend;
+            
+                return sub1;
+            
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var add1 = number1 + number2;
+
+            return add1;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
+            //public long Multiply(long factor 1, int factor2
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var mul1n = factor1 * factor2;
+
+            return mul1n;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (nameOfPerson.Length > 0)
+            {
+                return "Hello, " + nameOfPerson + "!";
+            }
+            else
+            {
+                return "Hello!";
+            }
+
         }
 
         public string GetHey()
